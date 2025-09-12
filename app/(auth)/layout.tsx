54a4cb8f4b8e2adeeaab8130346068767cd3d0b8
kunal rathore <kunal.rathore@ciphercru.com>
2025-09-12 11:44:37 +0530

    fixed auth layout css

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AuthLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className='h-full flex items-center justify-center mt-50'>
+    <div className='h-full flex items-center justify-center'>
         {children}
     </div>
   )
